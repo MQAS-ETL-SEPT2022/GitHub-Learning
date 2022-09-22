ec7c51ff07b6090eadf5b89b3faed7d0019ba16d
Pooja R <Pooja R@DESKTOP-UAO1U67>
2022-09-22 15:04:13 +0530

    Automation script on Checkbox module is updated

---
+++ GitHub_Learning/src/test/java/toolsqa/TC_02_Tejaswini_CheckBox.java
@@ -0,0 +1,40 @@
+package toolsqa;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC_02_Tejaswini_CheckBox {
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		System.out.println("webdriver setup is successfully");
+		
+        //Launching the Browser
+		WebDriver driver = new ChromeDriver();
+		System.out.println("Browser launched successfully");
+
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+        //Maximize the Browser
+		driver.manage().window().maximize();
+		System.out.println("Browser is maximized");
+		
+        //Launching the application
+		driver.get("https://demoqa.com/checkbox");
+		System.out.println("Application launched successfully");
+		
+        //Clicking the checkbox module
+		driver.findElement(By.className("rct-checkbox")).click();
+		System.out.println("successfully clicked on checkbox module");
+
+		driver.close();
+		System.out.println("Application closed successfully");
+
+	}
+}
