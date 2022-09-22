b5c248ceb7f5c2edc835c87e643b8478f31b4c3c
User <User@DESKTOP-85FK620>
2022-09-22 16:05:14 +0530

    Merge branch 'master' of https://github.com/MQAS-ETL-SEPT2022/GitHub-Learning.git

--- GitHub_Learning/src/test/java/toolsqa/TC_06_Preeti_Button.java
+++ GitHub_Learning/src/test/java/toolsqa/TC_06_Preeti_Button.java
@@ -2,25 +2,18 @@ package toolsqa;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriver.Timeouts;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class TC_06_Preeti_Button {
+public class TC_06_Preeti_Button extends Basetest{
 
-	public static void main(String[] args) {
-
-		// launching the application
-		WebDriverManager.chromedriver().setup();
-		System.out.println("WebDriver Manager Setup Successfully");
-
-		// Launching chrome browser
-		WebDriver driver = new ChromeDriver();
-		System.out.println("Chrome driver object created for launching browser");
+	@Test
+	   public void testcase(){
 
 		// Fetching the url
 		driver.get("https://demoqa.com/buttons");
@@ -33,25 +26,19 @@ public class TC_06_Preeti_Button {
 		// Find out the web element and store in reference variable and perform action
 		// on it
 		WebElement double_click = driver.findElement(By.id("doubleClickBtn"));
-		System.out.println("Successfully written xpath for doubleclick ");
 		action.doubleClick(double_click).build().perform();
-		System.out.println("Successfully double click ");
+		System.out.println("Successfully double clicked ");
 
 		// Find out the web element and store in reference variable and perform action
 		// on it
 		WebElement rightClick = driver.findElement(By.id("rightClickBtn"));
-		System.out.println("Successfully findout rightclick webelement ");
-		action.contextClick(rightClick).perform();
+		action.contextClick(rightClick).build().perform();
 		System.out.println("Successfully right click ");
 
 		// Find out the web element and store in reference variable and perform action
 		// on it
 		driver.findElement(By.xpath("//button[text()='Click Me']")).click();
-		System.out.println("Successfully written xpath for click me ");
-
-		// Finally closing the browser
-		driver.quit();
-		System.out.println("Chrome driver closed successfully");
+		System.out.println("Successfully clicked on click me ");
 
 	}
 }
