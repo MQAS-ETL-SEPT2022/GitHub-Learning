091a81d522ba142e7a259a86fa0c416287596203
User <User@DESKTOP-85FK620>
2022-09-22 16:15:23 +0530

    Updated Automation TestScript on Buttons

---
+++ GitHub_Learning/src/test/java/toolsqa/TC_06_Preeti_Buttons.java
@@ -0,0 +1,44 @@
+package toolsqa;
+
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.WebElement;
+	import org.openqa.selenium.chrome.ChromeDriver;
+	import org.openqa.selenium.interactions.Actions;
+	import org.testng.Reporter;
+	import org.testng.annotations.Test;
+
+	import io.github.bonigarcia.wdm.WebDriverManager;
+	public class TC_06_Preeti_Buttons extends Basetest{
+	
+		@Test
+		   public void testcase(){
+
+			// Fetching the url
+			driver.get("https://demoqa.com/buttons");
+			System.out.println("Chrome driver launched successfully");
+
+			// Creating instance of action class and passing the driver as sub instance
+			Actions action = new Actions(driver);
+			System.out.println("Created object of action class and passing the driver instance");
+
+			// Find out the web element and store in reference variable and perform action
+			// on it
+			WebElement double_click = driver.findElement(By.id("doubleClickBtn"));
+			action.doubleClick(double_click).build().perform();
+			System.out.println("Successfully double clicked ");
+
+			// Find out the web element and store in reference variable and perform action
+			// on it
+			WebElement rightClick = driver.findElement(By.id("rightClickBtn"));
+			action.contextClick(rightClick).build().perform();
+			System.out.println("Successfully right click ");
+
+			// Find out the web element and store in reference variable and perform action
+			// on it
+			driver.findElement(By.xpath("//button[text()='Click Me']")).click();
+			System.out.println("Successfully clicked on click me ");
+
+		}
+	}
+
