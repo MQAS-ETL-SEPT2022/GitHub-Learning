7d3468f7099200df6d63686d34f1390711cb212a
Dell <Dell@DESKTOP-7HJ0FFF>
2022-09-22 16:59:31 -0700

    Updated automation script of upload and download module TC_01_Priya_UpdAndDownload

--- GitHub_Learning/src/test/java/toolsqa/TC_01_Priya_UpdAndDownload.java
+++ GitHub_Learning/src/test/java/toolsqa/TC_01_Priya_UpdAndDownload.java
@@ -1,5 +1,7 @@
 package toolsqa;
 
+import static org.testng.Assert.assertTrue;
+
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -9,48 +11,53 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class TC_01_Priya_UpdAndDownload {
-
-	public static void main(String[] args) throws InterruptedException {
+public class TC_01_Priya_UpdAndDownload extends Basetest {
+	@Test
+	public void testcase() throws InterruptedException {
 		// Launching the browser
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
-		System.out.println("Browser launched successfully");
+		Reporter.log("Browser launched successfully",true);
 		Thread.sleep(2000);
 
 		// Maximising the browser
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		WebDriverWait wait = new WebDriverWait(driver, 20);
 		driver.manage().window().maximize();
-		System.out.println("Browser maximised successfully");
+		Reporter.log("Browser maximised successfully",true);
 		Thread.sleep(2000);
 
 		// Launching the application
 		driver.get("https://demoqa.com/links");
-		System.out.println("Application launched successfully");
+		Reporter.log("Application launched successfully",true);
 		Thread.sleep(2000);
+		
 
 		// Clicking on elements
 		driver.findElement(By.xpath("//div[@class='element-group']")).click();
-		System.out.println("Clicked on Element button successfully");
+		Reporter.log("Clicked on Element button successfully",true);
 		Thread.sleep(2000);
+		
+		
 
 		// Performing action
 		Actions action = new Actions(driver);
 		action.sendKeys(Keys.PAGE_DOWN).perform();
-		System.out.println("Action performed succesfully");
+		Reporter.log("Action performed succesfully",true);
 		Thread.sleep(5000);
 
 		// Clicking on upload and download option
 		driver.findElement(By.xpath("//span[text()='Upload and Download']")).click();
-		System.out.println("Clicked on Upload and Download");
+		Reporter.log("Clicked on Upload and Download",true);
 
 		// Closing the application
 		driver.close();
-		System.out.println("Application is closed successfully");
+		Reporter.log("Application is closed successfully",true);
 
 	}
 
