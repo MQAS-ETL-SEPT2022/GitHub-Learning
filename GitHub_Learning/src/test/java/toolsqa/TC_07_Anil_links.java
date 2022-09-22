c39b63f271bd1b2a8deca1d29e7a9e84d6463176
Anilkumarsm28 <anilkumarsm28@gmail.com>
2022-09-22 18:12:33 +0530

    Updated POM.xml

--- GitHub_Learning/src/test/java/toolsqa/TC_07_Anil_links.java
+++ GitHub_Learning/src/test/java/toolsqa/TC_07_Anil_links.java
@@ -1,27 +1,24 @@
 package toolsqa;
 
-	import org.openqa.selenium.By;
-	import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
 
 import baseclass.Basetest;
 
+public class TC_07_Anil_links extends Basetest {
 
+	@Test
+	public void testcase1() throws InterruptedException {
+		// opening demoQA page
+		driver.get("https://demoqa.com/");
 
-	public class TC_07_Anil_links extends Basetest {
+		// opening links page
+		driver.get("https://demoqa.com/links");
+		System.out.println("Links page opened successfully");
 
-		@Test
-		public void testcase1() throws InterruptedException {
-			//opening demoQA page
-			driver.get("https://demoqa.com/");
-			
-			// opening links page
-			driver.get("https://demoqa.com/links");
-			System.out.println("Links page opened successfully");
-
-			// clicking on "Home" link
-			driver.findElement(By.id("simpleLink")).click();
-			System.out.println("clicked on Home link susccessfully");
-			
-		}
+		// clicking on "Home" link
+		driver.findElement(By.id("simpleLink")).click();
+		System.out.println("clicked on Home link susccessfully");
+	}
 
 }
