005cf06f8c2354130b8c9b530fd882c1b5199469
Anilkumarsm28 <anilkumarsm28@gmail.com>
2022-09-22 15:17:01 +0530

    updated base test

--- GitHub_Learning/src/test/java/toolsqa/Basetest.java
+++ GitHub_Learning/src/test/java/toolsqa/Basetest.java
@@ -11,23 +11,25 @@ import org.testng.annotations.BeforeClass;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Basetest {
-   
-    WebDriver driver;
-    @BeforeClass
-    public void launchbrowser() {
-    	//launching the browser
-    	WebDriverManager.chromedriver().setup();
-		 driver = new ChromeDriver();
-		 Reporter.log("Browser is launched"); 
+
+	WebDriver driver;
+
+	@BeforeClass
+	public void launchbrowser() {
+		// launching the browser
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		Reporter.log("Browser is launched");
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		Reporter.log("Browser is maxmixed");
-    	
-    }
-    @AfterClass
-    public void closeBrowser() {
-    	//Closing the browser
-    	driver.close();
-    	Reporter.log("Browser is successfully closed");
-    }
+
+	}
+
+	@AfterClass
+	public void closeBrowser() {
+		// Closing the browser
+		driver.close();
+		Reporter.log("Browser is successfully closed");
+	}
 }
